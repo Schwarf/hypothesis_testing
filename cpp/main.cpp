17beaee82a7f19ce0fa0c91c14fbc4ba02d0508a
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-03-18 10:41:17 +0100

    WIP

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include "read_data/csv_data_reader.h"
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	MyTemplate<int> myObj(42);
+	myObj.printVal();
+	std::string path{"/media/linux_data/projects/cpp/hypothesis_testing/data/body_fat.csv"};
+	auto reader = CSVDataReader<int, double, std::string>(path);
+
 	return 0;
+
 }
--- cpp/read_data/csv_data_reader.h
+++ cpp/read_data/csv_data_reader.h
@@ -8,16 +8,25 @@
 #include <filesystem>
 #include <tuple>
 #include <vector>
+#include <fstream>
+#include <sstream>
+
 template <typename ...DataRows >
 class CSVDataReader
 {
 public:
-	explicit CSVDataReader(std::string & path);
+	explicit CSVDataReader(std::string & path, bool head_header =true);
 	void read();
+	std::vector<std::tuple<DataRows...>> get_data() const;
+	std::vector<std::string> get_header() const;
+
 private:
 	std::vector<std::string> column_names_;
 	std::vector<std::tuple<DataRows...>> data_;
+	std::filesystem::path path_;
+	bool has_header_;
 };
 
+#include "csv_data_reader.inl"
 
 #endif //CSV_DATA_READER_H
