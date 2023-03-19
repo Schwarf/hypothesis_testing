97ba14a61778a95b0466bb998da6bf2583d5c31f
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-03-19 19:40:54 +0100

    The literature sucks.

--- python/fuel_costs/descriptive_statistics.py
+++ python/fuel_costs/descriptive_statistics.py
@@ -1,12 +1,15 @@
+import matplotlib.pyplot as plt
 import numpy
-import constants
+import scipy.stats as stats
 
+import constants
 
 
 def compute_mean_and_std(data: numpy.array, axis: int, result_index: int) -> numpy.array:
     mean = numpy.mean(data, axis=axis)[result_index]
     standard_deviation = numpy.std(data, axis=axis, ddof=1)[result_index]
-    return numpy.array([mean, standard_deviation])
+    standard_error = standard_deviation / numpy.sqrt(data.shape[axis])
+    return numpy.array([mean, standard_deviation, standard_error])
 
 
 def read_data() -> numpy.array:
@@ -14,5 +17,7 @@ def read_data() -> numpy.array:
     return data
 
 
-
-
+def sampling_points_normal_distribution(mean: float, standard_deviation: float):
+    x = numpy.linspace(260. - 4*31, 260 + 4*31, 1000)
+    plt.plot(x, stats.t.pdf(x, 24, loc=260, scale=31/numpy.sqrt(24)))
+    plt.show()
--- python/main.py
+++ python/main.py
@@ -7,6 +7,7 @@ import fuel_costs.descriptive_statistics
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     data =descriptive_statistics.read_data()
-    print(descriptive_statistics.compute_mean_and_std(data, axis=0, result_index=1))
-
+    mean, standard_deviation, standard_error = descriptive_statistics.compute_mean_and_std(data, axis=0, result_index=1)
+    print(mean, ", ",standard_deviation, ", ", standard_error)
+    descriptive_statistics.sampling_points_normal_distribution(mean, standard_deviation)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
